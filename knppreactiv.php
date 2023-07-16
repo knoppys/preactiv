063028a89c1ce638bff2164d96e6c0e116f63a04
Alex Knopp <coda@knoppys.co.uk>
2023-07-16 06:44:28 +0100

    Fix plugin header

--- knppreactiv.php
+++ knppreactiv.php
@@ -1,11 +1,12 @@
 <?php
 /*
- * Plugin Name:       Knp PreActive Customisations
- * Description:       Handle the logic for form submissions, courses etc and hadnle the encrypted user profiles
- * Version:           1.0.0
- * Requires at least: 5.2
- * Requires PHP:      7.2
- */
+Plugin Name: Knp PreActiv
+Plugin URI: https://github.com/knoppys/preactiv
+Description: Course logic and patient profiles
+Version: 1
+Author: Alex Knopp
+Author URI: https://www.knoppys.co.uk
+*/
 
 require dirname(__FILE__).'/vendor/autoload.php';
 
